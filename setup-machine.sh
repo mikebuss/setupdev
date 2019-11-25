a52d916f8d24254c5234490292fc10491c6c4b3c
Mike Buss <mike@mikebuss.com>
2019-11-25 14:01:17 -0500

    Updated to modern tools

--- setup-machine.sh
+++ setup-machine.sh
@@ -3,48 +3,48 @@ brew tap caskroom/cask
 
 # Setup macOS
 # Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
+#sudo nvram SystemAudioVolume=" "
 # Disable Resume system-wide
-defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+#defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
 # Never go into computer sleep mode
-sudo systemsetup -setcomputersleep Off > /dev/null
+#sudo systemsetup -setcomputersleep Off > /dev/null
 # Check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+#defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 # Disable smart quotes as they’re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+#defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 # Disable smart dashes as they’re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+#defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 # Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+#defaults write com.apple.screensaver askForPassword -int 1
+#defaults write com.apple.screensaver askForPasswordDelay -int 0
 # Enable two-button mouse (requires restart)
-defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
+#defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
 # Don’t display the annoying prompt when quitting iTerm
-defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+#defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
 # GUI Applications
 brew cask install --appdir="~/Applications" iterm2
 brew cask install --appdir="/Applications" slack
 brew cask install --appdir="/Applications" 1password
-brew cask install --appdir="/Applications" skype
-brew cask install --appdir="/Applications" google-backup-and-sync
-brew cask install --appdir="/Applications" macdown
-brew cask install --appdir="/Applications" simplenote
-brew cask install --appdir="/Applications" bartender
+#brew cask install --appdir="/Applications" skype
+#brew cask install --appdir="/Applications" google-backup-and-sync
+#brew cask install --appdir="/Applications" macdown
+#brew cask install --appdir="/Applications" simplenote
+#brew cask install --appdir="/Applications" bartender
 brew cask install --appdir="/Applications" sketch
 brew cask install --appdir="/Applications" numi
-brew cask install --appdir="/Applications" spotify
+#brew cask install --appdir="/Applications" spotify
 brew cask install --appdir="/Applications" transmit
 brew cask install --appdir="/Applications" tripmode
 brew cask install --appdir="/Applications" vlc
 brew cask install --appdir="/Applications" caffeine
 
 # Apps from the Mac App Store
-mas install 904280696 # Things 3
+#mas install 904280696 # Things 3
 mas install 924726344 # Deliveries
 mas install 975937182 # Fantastical 2
-mas install 578071639 # Promotee
-mas install 425955336 # Skitch
+#mas install 578071639 # Promotee
+#mas install 425955336 # Skitch
 mas install 935235287 # Encrypto
 
 # Setup Oh-my-zsh
